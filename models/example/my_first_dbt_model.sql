3a4744ba3ff7a5179319f61e0f8ab76d68142c78
lllong33 <lllong33new@gmail.com>
2023-07-18 10:49:45 +0800

    fix

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -11,9 +11,13 @@
 
 with source_data as (
 
-    select 1 as id
+    select 1 as id, '20230706' as dt
     union all
-    select null as id
+    select 2 as id, '20230707' as dt
+    union all
+    select 2 as id, '20230708' as dt
+    union all
+    select 2 as id, '20230706' as dt
 
 )
 
