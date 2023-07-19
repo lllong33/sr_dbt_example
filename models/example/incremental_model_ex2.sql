a0dcc3c4338a7bf92bc17d24c39a8f054f5c7d4b
root <root@node3.localdomain>
2023-07-19 14:47:23 +0800

    安装sr=3.1测试dbt-core=1.5.2, disable model dir

--- models/example/incremental_model_ex2.sql
+++ models/example/incremental_model_ex2.sql
@@ -1,11 +1,12 @@
 
 {{ config(materialized='incremental'
     ,engine='OLAP'
+    ,keys=["id"]
+    ,table_type='PRIMARY'
     ,distributed_by=['id']
-    ,partition_by= ['dt']
-    ,partition_by_init= ["PARTITION p1 VALUES [('1971-01-01 00:00:00'), ('1991-01-01 00:00:00')),PARTITION p1972 VALUES [('1991-01-01 00:00:00'), ('1999-01-01 00:00:00')), PARTITION p2099 VALUES [('1999-01-01 00:00:00'), ('2099-01-01 00:00:00'))"]
-    ) 
+)
     }}
+
 
 SELECT 1 as id, cast('2023-07-06' as datetime) as dt    ,from_unixtime(unix_timestamp(), 'yyyy-MM-dd HH:mm:ss')   as etl_datetime  -- ETL时间
 union all 
